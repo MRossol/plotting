22c62d5b50a4adc101c4489c597055fe44ff457e
Michael Rossol <michael.rossol@nrel.gov>
2018-01-25 20:31:00 -0700

    refactor v2

--- plotting/lines.py
+++ plotting/lines.py
@@ -5,7 +5,7 @@ import itertools
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
-from .base import plotting_base
+import seaborn as sns
 
 COLORS = {
     "red": (0.7176, 0.1098, 0.1098),
@@ -69,6 +69,111 @@ def riffle_lines(*args):
     Flattened list of lists such that entries are riffled
     """
     return [item for sublist in zip(*args) for item in sublist]
+
+
+def lines_base(plot_func, *args, despine=True,
+               figsize=(6, 5), dpi=100, fontsize=14,
+               xlabel=None, ylabel=None, xlim=None, ylim=None,
+               xticks=None, yticks=None, ticksize=(6, 1),
+               xtick_labels=None, ytick_labels=None, borderwidth=1,
+               title=None, legend=None, filename=None, **kwargs):
+    """
+    Parameters
+    ----------
+    plot_func : 'function'
+        Plotting function
+    * args
+        Args for plot_func
+    despine : 'bool'
+        Despine axis in seaborn
+    figsize : 'Tuple', default = '(8,6)'
+        Width and height of figure
+    dpi : 'Int', default = '100'
+        DPI resolution of figure.
+    fontsize : 'Int'
+        Font size to be used for axes labels, ticks will be 2 points smaller
+    xlabel : 'String'
+        Label for x-axis.
+    ylabel : 'String'
+        Label for y-axis.
+    xlim : 'tuple', len(xlim) == 2
+        Upper and lower limits for the x-axis.
+    ylim : 'tuple', len(ylim) == 2
+        Upper and lower limits for the y-axis.
+    xticks : 'ndarray'
+        List of ticks to use on the x-axis. Should be within the bounds set by
+        xlim.
+    yticks : 'ndarray'
+        List of ticks to use on the y-axis. Should be within the bound set by
+        ylim.
+    ticksize : 'ndarray', default '[8,2]'
+        Length and width of ticks.
+    xtick_labels : 'list'
+        List of custome xticks. Note len(xticks) == len(xtick_labels)
+    ytick_labels : 'list'
+        List of custome yticks. Note len(yticks) == len(ytick_labels)
+    boarderwidth : 'Int'
+        Linewidth of plot frame
+    title : 'str'
+        Plot title
+    legend : 'list'
+        legend labels
+    filename : 'String', default = None.
+        Name of file/path to save the figure to.
+    **kwargs
+        kwargs for plot_func
+    """
+    fig = plt.figure(figsize=figsize, dpi=dpi)
+    axis = fig.add_subplot(111)
+
+    plot_func(axis, *args, **kwargs)
+
+    if despine:
+        sns.despine(offset=10, trim=False)
+
+    if title is not None:
+        axis.set_title(title, fontsize=fontsize + 2)
+
+    if xlabel is not None:
+        axis.set_xlabel(xlabel, fontsize=fontsize)
+
+    if ylabel is not None:
+        axis.set_ylabel(ylabel, fontsize=fontsize)
+
+    if xlim is not None:
+        axis.set_xlim(xlim)
+
+    if ylim is not None:
+        axis.set_ylim(ylim)
+
+    axis.tick_params(axis='both', labelsize=fontsize - 2,
+                     width=ticksize[1], length=ticksize[0])
+
+    if xticks is not None:
+        axis.set_xticks(xticks)
+        if xtick_labels is not None:
+            axis.set_xticklabels(xtick_labels)
+
+    if yticks is not None:
+        axis.set_yticks(yticks)
+        if ytick_labels is not None:
+            axis.set_yticklabels(ytick_labels)
+
+    for ax in ['top', 'bottom', 'left', 'right']:
+        axis.spines[ax].set_linewidth(borderwidth)
+
+    if legend is not None:
+        plt.legend(legend, bbox_to_anchor=(1.05, 1), loc=2,
+                   borderaxespad=0., prop={'size': fontsize - 2})
+
+    fig.tight_layout()
+    if filename is not None:
+        plt.savefig(filename, dpi=dpi, transparent=True,
+                    bbox_inches='tight')
+    else:
+        plt.show()
+
+    plt.close()
 
 
 def line_plot(data, **kwargs):
@@ -147,7 +252,7 @@ def line_plot(data, **kwargs):
                       mec=mec, mew=mew, color=next(colors),
                       linestyle=next(linestyles), linewidth=linewidth)
 
-    plotting_base(plot_func, data, **kwargs)
+    lines_base(plot_func, data, **kwargs)
 
 
 def error_plot(data, **kwargs):
@@ -235,7 +340,7 @@ def error_plot(data, **kwargs):
                           linestyle=next(linestyles), mec=mec, mew=mew,
                           capsize=capsize, capthick=linewidth)
 
-    plotting_base(plot_func, data, **kwargs)
+    lines_base(plot_func, data, **kwargs)
 
 
 def dual_plot(data1, data2,
