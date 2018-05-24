41e02682f76319264c6cb59e7d84b5df00a9558a
Michael Rossol <michael.rossol@nrel.gov>
2018-05-24 09:18:42 -0600

    setup.py added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='plotting',
+    version='0.1.0',
+    packages=['plotting', ],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
