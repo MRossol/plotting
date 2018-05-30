bb5f9c696345451100aea7739c6f60dfbe9486bb
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-05-29 22:49:48 -0600

    Reformat setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 from setuptools import setup, find_packages
 
-install_requires = [
-    "matplotlib",
-    "seaborn",
-]
+install_requires = ["matplotlib", "seaborn"]
 
 setup(
-    name='plotting',
-    version='0.1.0',
-    author='Michael Rossol',
-    author_email='mrossol@gmail.com',
-    long_description=open('README.md').read(),
+    name="plotting",
+    version="0.1.0",
+    author="Michael Rossol",
+    author_email="mrossol@gmail.com",
+    long_description=open("README.md").read(),
     packages=find_packages(),
     install_requires=install_requires,
 )
