982c66ddc706457e454f762411b2defcc7d00694
Michael Rossol <mrossol@gmail.com>
2020-02-13 14:16:24 -0700

    update package base: setup.py verison.py license pre-commit files

---
+++ README.rst
@@ -0,0 +1,28 @@
+plotting
+********
+A plotting wrapper on matplotlib and seaborn to enable single function plotting calls a la mathematica
+
+Installation
+==============
+
+.. code-block:: bash
+
+    pip install plotting
+
+Developement
+============
+
+If you want to contribute to this package:
+
+1. Clone the repository: ``git clone https://github.com/mrossol/plotting.git``
+    1) enter github username
+    2) enter github password
+
+2. Install the pacakge
+    1) cd into plotting repo cloned above
+    2) run ``pip install -e .``
+
+3. Check that plotting was installed successfully
+    .. code-block:: python
+
+        import plotting
--- setup.py
+++ setup.py
@@ -1,13 +1,69 @@
+"""
+setup.py
+"""
+import os
+from codecs import open
 from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from subprocess import check_call
+import shlex
+from warnings import warn
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open("README.rst", encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
+with open(os.path.join(here, "plotting", "version.py"), encoding="utf-8") as f:
+    version = f.read()
+
+version = version.split('=')[-1].strip().strip('"').strip("'")
+
+
+class PostDevelopCommand(develop):
+    """
+    Class to run post setup commands
+    """
+
+    def run(self):
+        """
+        Run method that tries to install pre-commit hooks
+        """
+        try:
+            check_call(shlex.split("pre-commit install"))
+        except Exception as e:
+            warn("Unable to run 'pre-commit install': {}"
+                 .format(e))
+
+        develop.run(self)
+
 
 install_requires = ["matplotlib", "seaborn"]
 
 setup(
     name="plotting",
-    version="0.0.1",
+    version=version,
     author="Michael Rossol",
     author_email="mrossol@gmail.com",
-    long_description=open("README.md").read(),
+    long_description=readme,
     packages=find_packages(),
+    package_dir={"plotting": "plotting"},
+    include_package_data=True,
+    license="BSD license",
+    zip_safe=False,
+    keywords="rev",
+    classifiers=[
+        "Development Status :: Beta",
+        "Intended Audience :: Modelers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
     install_requires=install_requires,
+    extras_require={
+        "dev": ["flake8", "pre-commit", "pylint"],
+    },
+    cmdclass={"develop": PostDevelopCommand},
 )
