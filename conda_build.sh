870b442a14dcf08db9603eb1d3cc847f7936f8da
Michael Rossol <mrossol@gmail.com>
2020-05-20 13:06:07 -0600

    clean up conda build

--- conda_build.sh
+++ conda_build.sh
@@ -4,33 +4,27 @@ set -e
 
 PKG_NAME=plotting
 
-ARRAY=( 3.6 3.7 3.8 )
+PY_VERION=( 3.6 3.7 3.8 )
 
 export CONDA_BLD_PATH=~/conda-bld
 
-for i in "${ARRAY[@]}"
-do
-	conda-build conda.recipe/ --python $i
-done
-
-# convert package to other platforms
 platforms=( osx-64 linux-64 win-64 )
-find $CONDA_BLD_PATH/ -name $PKG_NAME*.tar.bz2 | while read file
+for py in "${PY_VERION[@]}"
 do
-    echo $file
-    #conda convert --platform all $file  -o $HOME/conda-bld/
+	conda build conda.recipe/ --python=$py
+    file=$(conda build conda.recipe/ --python=$py --output)
     for platform in "${platforms[@]}"
     do
-       conda convert --platform $platform $file  -o $CONDA_BLD_PATH/
+       conda convert --platform $platform $file -o $CONDA_BLD_PATH/
     done
 done
 
 # upload packages to conda
 find $CONDA_BLD_PATH/ -name $PKG_NAME*.tar.bz2 | while read file
 do
-    echo $file
-    anaconda upload -u mrossol $file
+    anaconda upload $file
 done
 
 echo "Building and uploading conda package done!"
 rm -rf $CONDA_BLD_PATH/*
+ls $CONDA_BLD_PATH
